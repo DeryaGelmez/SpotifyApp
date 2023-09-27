6e639d977c5c7fb6939650a15723efe637c488f3
derya <derya@DERYA>
2023-09-27 18:46:55 +0300

    Add project files.

---
+++ Spotify/Program.cs
@@ -0,0 +1,40 @@
+﻿using Spotify.Entities;
+using Spotify.Services;
+using System.ComponentModel.DataAnnotations;
+
+namespace Spotify
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Spotify-Music for Noone");
+
+            string currentDirectory=Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.Parent?.FullName;
+            Console.WriteLine();
+
+            var song1 = new Song("Requiem K. 626", "W.A. Mozart", "Orchestre national de France", null);
+            var song2 = new Song("Sonata No.6 in F Major", "L. Beethoven", "Fazıl Say", "Warner Music");
+            var song3 = new Song("Moonlight Sonata", "Ludwig van Beethoven", "Various Artists", "ClassicalRecords");
+            var song4 = new Song("The Flour Seasons", "Antonio Vivaldi", "Chamber Orchestra", "Baroque Productions");
+            var song5 = new Song("Symphony No. 9", "Ludwig van Beethoven", "Philharmonic Orchestra", "Classical Harmony Records");
+
+            var playlist1 = new Playlist(song1);
+            playlist1.AddSong(song2);
+            playlist1.AddSong(song3);
+            playlist1.AddSong(song4);
+            playlist1.AddSong(song5);
+
+
+            Console.WriteLine("Before Shuffle:");
+            Console.WriteLine(playlist1.GetSongs());
+            playlist1.ShuffleSongs();
+            Console.WriteLine("\n\nAfter Shuffle:");
+            Console.WriteLine(playlist1.GetSongs());
+
+            NotepadService notepadService = new();
+            notepadService.WriteToNotepad(playlist1);
+
+        }
+    }
+}
---
+++ Spotify/Services/NotepadService.cs
@@ -0,0 +1,25 @@
+﻿using Spotify.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify.Services
+{
+    internal class NotepadService
+    {
+        public void PlaylistToNotepad(Playlist playlist)
+        {
+            //Saving To Notepad...
+            string directory = @$"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.Parent?.FullName}\Playlists";
+
+            if (!File.Exists(directory))
+                Directory.CreateDirectory(directory);
+
+            string saveFile = Path.Combine(directory, $"{playlist.Id}.txt");
+            File.WriteAllText(saveFile,playlist.GetSongs());
+            Console.WriteLine("Data succesfully saved to notepad ");
+        }
+    }
+}
